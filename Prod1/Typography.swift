b55aecb70da307b04d76c8158f1abf7bbbf72c33
KTApps <mahajakelvin@gmail.com>
2025-10-06 17:39:57 +0100

    [Task] Created Typography that reuses font sizes in the authentication views

---
+++ Prod1/Typography.swift
@@ -0,0 +1,16 @@
+//
+//  Typography.swift
+//  Prod1
+//
+//  Created by Kelvin Mahaja on 06/10/2025.
+//
+
+import SwiftUI
+
+// MARK: Typography namespace within AuthState
+extension AuthState {
+    enum Typography {
+        static var font_1_auth_bold: Font { .system(size: 20, weight: .bold) }
+        static var font_1_auth_light: Font { .system(size: 20, weight: .light) }
+    }
+}
